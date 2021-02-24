6a9a1cb85098a438722920369a478f6e00e6d40e
Narjesse <narjess.derouich@gmail.com>
2021-02-24 17:38:43 +0100

    added microservice

---
+++ src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
@@ -0,0 +1,36 @@
+package com.ecommerce.microcommerce.web.controller;
+import com.ecommerce.microcommerce.dao.*;
+import com.ecommerce.microcommerce.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class ProductController {
+
+    @Autowired
+    private ProductDao productDao;
+
+        @RequestMapping(value="/products", method= RequestMethod.GET)
+        public List<Product> listeProduits() {
+            return productDao.findAll();
+        }
+
+    @GetMapping(value = "/products/{id}")
+    public Product afficherUnProduit(@PathVariable int id) {
+
+        return productDao.findById(id);
+
+
+    }
+
+    //ajouter un produit
+    @PostMapping(value = "/products")
+    public void ajouterProduit(@RequestBody Product product) {
+        productDao.save(product);
+    }
+
+
+}
+
